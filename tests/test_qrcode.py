95c65b1e2ecbc006f472601a6715931a68524e2e
as6325400 <as6325400@gmail.com>
2024-10-07 01:09:14 +0800

    update unittest

--- tests/test_qrcode.py
+++ tests/test_qrcode.py
@@ -10,22 +10,17 @@ from unittest.mock import patch
 class TestQrcode(unittest.TestCase):
     @patch('matplotlib.pyplot.imsave')
     def test_qrcodesave(self, mock_plt_save):
-        # 资源目录中的文件路径
         resource_dir = "tests"
         resource_file = "1.png"
         resource_path = os.path.join(resource_dir, resource_file)
 
-        # 确保资源文件存在
         self.assertTrue(os.path.exists(resource_path), f"Resource file {resource_path} does not exist")
 
-        # 创建临时目录
         with tempfile.TemporaryDirectory() as tmpdirname:
             temp_file = os.path.join(tmpdirname, resource_file)
 
-            # 将资源文件复制到临时目录中
             shutil.copy(resource_path, temp_file)
 
-            # 使用 NamedTemporaryFile 创建临时保存路径
             with tempfile.NamedTemporaryFile(suffix=".png", delete=True) as temp_output_file:
                 Qr = Qrcode("Accepted")
                 Qr.generate(6, "L", mask = 0, mode = "Normal")
@@ -46,12 +41,6 @@ class TestQrcode(unittest.TestCase):
                 Qr = Qrcode("Accepted")
                 Qr.generate(6, "Q", mask = 7, mode = "Normal")
                 num = Qr.InformationMask()
-                # Qr.showdata()
-                # # 创建 Image 对象并使用临时文件路径
-                # img = Image(temp_file)
-                # img.SetModuleNums(20)
-
-                # 保存到临时文件路径，文件会在测试结束后自动删除
                 Qr.save()
                 self.assertEqual(mock_plt_save.call_count, 3)
             with tempfile.NamedTemporaryFile(suffix=".png", delete=True) as temp_output_file:
